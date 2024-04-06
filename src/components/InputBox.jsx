6a83891dc78a7dcc03f5ddd21edb84f3f447e46c
piyushrai7271 <piyushrai7271@gmail.com>
2024-04-06 22:35:18 +0530

    basic bug fixed

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,4 +1,4 @@
-import React, {useId} from "react";
+import { useId } from "react";
 
 function InputBox({
   label,
@@ -29,7 +29,7 @@ function InputBox({
       </div>
       <div className="w-1/2 flex flex-wrap justify-end text-right">
         <p className="text-black/40 mb-2 w-full">Currency Type</p>
-        <select className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none" value={selectCurrency} onChange={(e)=>onCurrencyChange && onCurrencyChange(e.target.value)} disabled={currenDisable}>
+        <select className="rounded-lg px-1 py-1 bg-gray-600 cursor-pointer outline-none text-white" value={selectCurrency} onChange={(e)=>onCurrencyChange && onCurrencyChange(e.target.value)} disabled={currenDisable}>
           {currencyOptions.map((currency) => (
             <option key={currency} value={currency}>
                 {currency}
